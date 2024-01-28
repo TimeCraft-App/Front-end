3962c35ac3f32e26bae6446f81ee3f226e728c1f
Jeton Sllamniku <jetonsllamniku@gmail.com>
2024-01-28 23:13:24 +0100

    Redirect to login if not logged in

--- timecraft/src/App.js
+++ timecraft/src/App.js
@@ -1,26 +1,40 @@
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ReactNotifications } from "react-notifications-component";
 import 'react-notifications-component/dist/theme.css';
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes, Navigate } from "react-router-dom";
 import { AppContext } from "./AppContext";
 import Oops from "./Pages/Oops/Oops"
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
 
-
 function App() {
   const [isLoading, setIsLoading] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const jwtToken = localStorage.getItem("jwtToken");
+    if (jwtToken) {
+      setIsLoading(true);
+    }
+  }, []);
+
+  const HomeWithRedirect = () => {
+    if (!isLoggedIn) {
+      return <Navigate to="/login" />;
+    }
+    return <Home />;
+  };
+
   return (
     <>
       <AppContext.Provider value={{ isLoading, setIsLoading }}>
         <ReactNotifications />
         <Router>
           <Routes>
-            <Route path="/" element={<Home />} exact />
-            <Route path="/login" element={<Login />} exact />
-            <Route path="/register" element={<Register />} exact />
+            <Route path="/" element={<HomeWithRedirect />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
             <Route path="*" element={<Oops />} />
           </Routes>
         </Router>
@@ -28,4 +42,5 @@ function App() {
     </>
   );
 }
+
 export default App;
